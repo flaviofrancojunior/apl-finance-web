a277a4b77ed756909c22a75a8400b507e2d28b11
franfla <franfla>
2019-07-19 11:35:54 -0300

    padrozinação de CSS ajuste de rotas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -38,9 +38,7 @@ export const routes: Routes = [
   {
     path: '',
     component: DefaultLayoutComponent,
-    data: {
-      title: 'Home'
-    },
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'configuracoes',
